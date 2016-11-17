6d123dcb0dcca6f7fd0335118c0c2414f88eff24
Md Zahid Raza <zahid7292@gmail.com>
2016-11-17 23:29:43 +0530

    cimmit 1

--- src/main/java/com/example/vmi/config/AppConfig.java
+++ src/main/java/com/example/vmi/config/AppConfig.java
@@ -8,11 +8,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ComponentScan(basePackages = {"com.example.vmi"})
-@Import(DispatcherConfig.class)
+@Import(value = {DispatcherConfig.class,RepositoryRestMvcConfiguration.class})
 public class AppConfig extends WebMvcConfigurerAdapter{
 
    @Bean
